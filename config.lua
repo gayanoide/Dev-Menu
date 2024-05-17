6e61a230793ba52672ffd4bdc08ce96849a1de2d
_Emy_3 <68069714+gayanoide@users.noreply.github.com>
2024-05-17 16:39:02 +0200

    .

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config             = {}
+
+
+Config.dev = {
+	lspd = {
+		position = {x = -349.47, y = -264.00, z = 43.24},
+        heading = 255.67,
+		titre = "L.S.P.D",
+		desc = "Los Santos Police Department",
+		image = "https://i.goopics.net/lrn6yw.png"
+	},
+	sams = {
+		position = {x = 338.01, y = -588.98, z = 74.17},
+        heading = 311.37,
+		titre = "S.A.M.S",
+		desc = "San Andreas Medical Services",
+		image = "https://i.goopics.net/jvgdya.png"
+	},
+	safd = {
+		position = {x = -651.17, y = -93.67, z = 49.94},
+        heading = 359.10,
+		titre = "S.A.F.D",
+		desc = "San Andreas Fire Department",
+		image = "https://i.goopics.net/l4zuce.png"
+	},
+	bcso = {
+		position = {x = -495.15, y = 5952.46, z = 34.39},
+        heading = 317.44,
+		titre = "B.C.S.O",
+		desc = "Blaine County Sheriff Office",
+		image = "https://i.goopics.net/nr5544.png"
+	},
+
+	AdminRanks = {"admin","superadmin","owner","_dev","god"}, --- Change for your server
+}
